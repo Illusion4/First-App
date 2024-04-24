b3734a51330235aa016692d46c3523dd4056aef7
Illusion4 <vladyslavlintur@gmail.com>
2024-04-24 13:55:32 +0300

    init solution

---
+++ TaskManagement.Client/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
+
+export const routes: Routes = [
+    { path:'login', component: LogInPageComponent }];
